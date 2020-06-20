1b4248d66e9e1b2cfb72cdc16e914a4101033ceb
kwamkert <tomriddle477@gmail.com>
2020-06-20 23:53:57 +0000

    string compression added

--- src/com/codeinsyt/Main.java
+++ src/com/codeinsyt/Main.java
@@ -10,7 +10,8 @@ public class Main {
        // System.out.println(IsPermutation.run("hello","lloh"));
        // System.out.println(Urlify.run("kwkame is nice      ", 14));
         //System.out.println(PalindromePermutation.run("Tapt Coa"));
-        System.out.println(OneWay.run("pale","bake"));
+      //  System.out.println(OneWay.run("pale","bake"));
+        System.out.println(String.valueOf(2).length() + 2);
     }
 
 }
---
+++ src/com/codeinsyt/arrays/StringCompression.java
@@ -0,0 +1,20 @@
+package com.codeinsyt.arrays;
+
+public class StringCompression {
+
+
+    public static  String run(String string){
+        int final_count = getFinalLength(string);
+    }
+
+
+    public static int getFinalLength(String string){
+        int counter = 0;
+        int compressLength = 0;
+
+        for(int i = 0; i < string.length(); i++){
+
+        }
+
+    }
+}
